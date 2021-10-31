a705c400302edf6aa7ecd3fb20d37ccf0de74e35
fl <fenglinmail@yahoo.com>
2021-10-31 19:13:06 -0400

    codebuilddemo sample project

---
+++ codebuilddemo/src/main/java/com/example/codebuilddemo/CodebuilddemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.codebuilddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebuilddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebuilddemoApplication.class, args);
+	}
+
+}
